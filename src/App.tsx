c7ac7783631ec2b2bedd3b185af638a0628595cd
Ronit-gurjar <ronitgurjar999@gmail.com>
2024-07-23 01:29:47 +0530

    Remove: unnecessary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Multistep from './components/multistep'
-import { Progress } from './components/ui/progress'
 
 import './App.css'
 
@@ -7,7 +6,6 @@ function App() {
 
   return (
     <>
-      <Progress/>
       <Multistep/>
     </>
   )
